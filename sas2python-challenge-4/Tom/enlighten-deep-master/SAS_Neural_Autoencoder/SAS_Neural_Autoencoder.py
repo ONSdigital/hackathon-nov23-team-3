2d912876a7026ae21d4e6c21abf5d9ddad457e63
TKeable <toby.keable@ons.gov.uk>
2023-11-27 11:39:34 +0000

    Created a wrong graph

--- sas2python-challenge-4/Tom/enlighten-deep-master/SAS_Neural_Autoencoder/SAS_Neural_Autoencoder.py
+++ sas2python-challenge-4/Tom/enlighten-deep-master/SAS_Neural_Autoencoder/SAS_Neural_Autoencoder.py
@@ -2,61 +2,63 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.neural_network import MLPRegressor
-from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics.pairwise import pairwise_distances
 import matplotlib.pyplot as plt
+from sas7bdat import SAS7BDAT
 import numpy as np
 
-# Define the necessary variables
-git_repo_dir = "D:\src-code\Hackthon\hackathon-nov23-team-3\sas2python-challenge-4\Tom\enlighten-deep-master\SAS_Neural_Autoencoder"  # Set your directory path
-num_cores = 1  # Set the number of cores
-num_outliers = 5  # Set the number of outliers to find
+def hidden_layer_output(model, X):
+    layer_output = X
+    for i in range(len(model.coefs_) - 1):
+        layer_output = np.maximum(0, np.dot(layer_output, model.coefs_[i]) + model.intercepts_[i])
+    return layer_output
 
+# Set GitHub data directory (must contain provider_summary.sas7bdat)
+git_repo_dir = "D:\GitHub\hackathon-nov23-team-3\sas2python-challenge-4\Tom\enlighten-deep-master\SAS_Neural_Autoencoder"  # Set your directory path
 
-# Load the data from a .sas7bdat file
-provider_summary = pd.read_sas(git_repo_dir + '\provider_summary.sas7bdat')
+# Set number of available cores
+num_cores = 1  # fill this with your number of cores
 
-# Get the column names
-column_names = provider_summary.columns.tolist()
+# Set the number of outliers to find (default=5)
+num_outliers = 5
 
-# Filter out 'provider_id' and 'name'
-name = [name for name in column_names if name not in ['provider_id', 'name']]
-print(name)
-# Now filtered_names contains the names of the columns in provider_summary, excluding 'provider_id' and 'name'
+# Load data
+with SAS7BDAT(f'{git_repo_dir}\provider_summary.sas7bdat') as file:
+    provider_summary = file.to_data_frame()
 
+# Filter columns
+cols_to_keep = [col for col in provider_summary.columns if col not in ['provider_id', 'name']]
+provider_summary = provider_summary[cols_to_keep]
 
-# # Load the data
-# provider_summary = pd.read_sas(git_repo_dir + "/provider_summary.sas7bdat")
-
-# # Filter the data to get the numeric inputs
-numeric_inputs = provider_summary.select_dtypes(include=['float64', 'int64']).drop(columns=['provider_id', 'name'])
-
-# Standardize the numeric inputs for K-means clustering
+# Standardize numeric inputs for K-means clustering
 scaler = StandardScaler()
-numeric_inputs_scaled = scaler.fit_transform(numeric_inputs)
+std_provider_summary = scaler.fit_transform(provider_summary)
 
 # K-means clustering
-kmeans = KMeans(n_clusters=16, random_state=0, n_jobs=num_cores).fit(numeric_inputs_scaled)
+kmeans = KMeans(n_clusters=16, max_iter=100, n_init=10, random_state=44444)
+kmeans.fit(std_provider_summary)
+
+# Add cluster labels to the data
+provider_summary['cluster'] = kmeans.labels_
 
 # Train simple stacked autoencoder with 5 hidden layers
-mlp = MLPRegressor(hidden_layer_sizes=(numeric_inputs.shape[1], numeric_inputs.shape[1]//2, 2, numeric_inputs.shape[1]//2, numeric_inputs.shape[1]), random_state=0)
-mlp.fit(numeric_inputs_scaled, numeric_inputs_scaled)
+autoencoder = MLPRegressor(hidden_layer_sizes=(5, provider_summary.shape[1], provider_summary.shape[1]//2, 2, provider_summary.shape[1]//2, provider_summary.shape[1], 5),
+                           max_iter=5000, random_state=44444)
+autoencoder.fit(std_provider_summary, std_provider_summary)
 
 # Score training data with trained neural network
-score2D = mlp.predict(numeric_inputs_scaled)
-
-# Keep output of 2-dimensional middle layer as new feature space
-middle_layer_output = mlp.hidden_layer_sizes[2]
+score2D = hidden_layer_output(autoencoder, std_provider_summary)
 
 # Set the median point as the origin of the new 2-D feature space
-origin = np.median(middle_layer_output, axis=0)
+origin = np.median(score2D, axis=0)
 
 # Calculate the distance of each point in new feature space from this origin
-distances = euclidean_distances(middle_layer_output, origin.reshape(1, -1))
+distances = pairwise_distances(score2D, origin.reshape(1, -1))
 
 # Detect outliers as first 5 points farthest from the origin
-outliers = distances.argsort()[-num_outliers:]
+outliers = np.argsort(distances, axis=0)[-num_outliers:]
 
 # Display the data in 2-D including cluster labels and outliers
-plt.scatter(middle_layer_output[:, 0], middle_layer_output[:, 1], c=kmeans.labels_)
-plt.scatter(middle_layer_output[outliers, 0], middle_layer_output[outliers, 1], c='red')
+plt.scatter(score2D[:, 0], score2D[:, 1], c=provider_summary['cluster'])
+plt.scatter(score2D[outliers, 0], score2D[outliers, 1], c='red')
 plt.show()
