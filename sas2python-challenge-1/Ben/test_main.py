36a36618f58ee1f2f7985b75bb3e34b1f4806ba7
lambeb <benjamin.lambe@ons.gov.uk>
2023-11-28 10:13:18 +0000

    Added some generated Cucumber scripts/Non working but good to see it can produce them

--- sas2python-challenge-1/Ben/test_main.py
+++ sas2python-challenge-1/Ben/test_main.py
@@ -18,6 +18,24 @@ class TestRemoveIncompletePeriods(unittest.TestCase):
         result = remove_incomplete_periods(df)
         self.assertEqual(len(result), 0)
 
+    def test_remove_incomplete_periods_with_no_complete_years(self):
+        df = pd.DataFrame({
+            'TS_PERIOD': ['2021Q1', '2021Q2', '2021Q3', '2021Q4', '2023Q1', '2023Q2'],
+            'DIMENSION1': ['value1', 'value1', 'value1', 'value1', 'value1', 'value1']
+        })
+        result = remove_incomplete_periods(df)
+        self.assertEqual(len(result), 0)
+
 if __name__ == '__main__':
     unittest.main()
+    Given a DataFrame with TS_PERIOD and DIMENSION1 columns
+    When remove_incomplete_periods function is called
+    Then the resulting DataFrame should only contain complete years with 4 quarters
+    And the length of the resulting DataFrame should be 4
 
+  Scenario: Remove incomplete periods from DataFrame with no complete years
+    Given a DataFrame with TS_PERIOD and DIMENSION1 columns
+    And the DataFrame does not contain any complete years with 4 quarters
+    When remove_incomplete_periods function is called
+    Then the resulting DataFrame should be empty
+    And the length of the resulting DataFrame should be 0
